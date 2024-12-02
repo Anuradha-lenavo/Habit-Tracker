952cc6aa5b0ef3795e8a20a1fe79d95163f2b010
Anuradha-lenavo <97876996+Anuradha-lenavo@users.noreply.github.com>
2024-12-02 21:05:41 +0530

    Update nodemailer.js

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -4,14 +4,14 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail', // or any other email service you want to use
   auth: {
-    user:  'rajeshwarim20004@gmail.com',
-        pass: 'vqaw uunk qgcv ozcb', // your email password or app password
+    user:  'YOUR EMAIL',
+        pass: 'YOUR EMAIL APP PASSWORD', // your email password or app password
   },
 });
 
 const sendReminderEmail = (email, habitName, reminderTime) => {
   const mailOptions = {
-    from:  'rajeshwarim20004@gmail.com',
+    from:  'YOUR EMAIL',
     to: email,
     subject: `Reminder for your habit: ${habitName}`,
     text: `This is a reminder for your habit "${habitName}" scheduled at ${reminderTime}. Don't forget to complete it!`,
