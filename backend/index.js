2bf1223127d45c18a8096606150e71b2d5020b6d
Anuradha-lenavo <97876996+Anuradha-lenavo@users.noreply.github.com>
2024-12-03 17:57:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const cronJobs = require('./cronJobs');
 app.use(express.json());
 app.use(cors());
 // Connect to MongoDB
-mongoose.connect('MONGO MONGODB URI', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1:27017/', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
